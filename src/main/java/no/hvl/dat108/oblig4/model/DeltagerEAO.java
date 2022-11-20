75831517364204baab3e1728d9c97ab50da5f0ae
Isak <595489@stud.hvl.no>
2022-11-20 00:01:03 -0400

    Renamed DeltakerEAO to DeltakerDB. Added TODOs

--- src/main/java/no/hvl/dat108/oblig4/model/DeltagerEAO.java
+++
@@ -1,4 +0,0 @@
-package no.hvl.dat108.oblig4.model;
-
-public class DeltagerEAO {
-}
--- src/test/java/no/hvl/dat108/oblig4/LagreDeltagerTest.java
+++ src/test/java/no/hvl/dat108/oblig4/LagreDeltagerTest.java
@@ -1,9 +1,9 @@
 package no.hvl.dat108.oblig4;
 
 import no.hvl.dat108.oblig4.model.Deltager;
+import no.hvl.dat108.oblig4.model.DeltagerDB;
 import no.hvl.dat108.oblig4.model.DeltagerJSON;
 import no.hvl.dat108.oblig4.model.DeltakerDAO;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -23,7 +23,8 @@ public class LagreDeltagerTest {
 
     Deltager deltager2;
     Deltager deltager3;
-    DeltakerDAO deltakerDAO = new DeltagerJSON();
+    DeltakerDAO saveJson = new DeltagerJSON();
+//    DeltakerDAO saveDB = new DeltagerDB(); //TODO implement database access class DeltagerDB
 
     @BeforeEach
     void setUp() {
@@ -31,15 +32,15 @@ public class LagreDeltagerTest {
         deltager2 = new Deltager("Hans", "Hansen", "Mann", "00030333", "SECONDHASH", "SECONDSALT");
         deltager3 = new Deltager("Lisa", "Listhaug", "Kvinne", "50055005", "LISAHASH", "LISASALT");
 
-        deltakerDAO.save(deltager);
-        deltakerDAO.save(deltager2);
-        deltakerDAO.save(deltager3);
+        saveJson.save(deltager);
+        saveJson.save(deltager2);
+        saveJson.save(deltager3);
     }
 
     @Test
     void testGet(){
-        System.out.println("Getter: " + deltakerDAO.get(mob) + " Comparer: " + deltager);
-        assertEquals(deltakerDAO.get(mob).toString(), deltager.toString());
+        System.out.println("Getter: " + saveJson.get(mob) + " Comparer: " + deltager);
+        assertEquals(saveJson.get(mob).toString(), deltager.toString());
     }
 
     @Test
@@ -49,6 +50,16 @@ public class LagreDeltagerTest {
         dl.add(deltager2);
         dl.add(deltager3);
 
-        assertEquals(deltakerDAO.getAll().toString(), dl.toString());
+        assertEquals(saveJson.getAll().toString(), dl.toString());
     }
+
+//    @Test
+//    void testGetDb(){
+//        //TODO implement test for database get
+//    }
+
+//    @Test
+//    void testGetAllDb(){
+//        //TODO implement test for database getAll
+//    }
 }
